dcad7c2d77d6f219dafab7306d5811a68068f731
frah90 <frah.221190@gmail.com>
2024-08-11 17:32:50 +0200

    Bugfix: dotnet install

--- first_setup/submodules/dev/dotnet_install.sh
+++ first_setup/submodules/dev/dotnet_install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Dotnet install (istructions from MS website https://learn.microsoft.com/en-us/dotnet/iot/deployment)
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel STS
+# Remove eventual pre-existing dotnet installations
+rm -rf $HOME/.dotnet
+sed -i '/export DOTNET_ROOT/d' $HOME/.bashrc
+sed -i '/export PATH=.*dotnet/d' $HOME/.bashrc
 
+# Dotnet install (istructions from MS website https://learn.microsoft.com/en-us/dotnet/iot/deployment and https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-install-script)
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current
+
+# Add dotnet to environment variables
 echo 'export DOTNET_ROOT=$HOME/.dotnet' >> $HOME/.bashrc
 echo 'export PATH=$PATH:$HOME/.dotnet' >> $HOME/.bashrc
 source $HOME/.bashrc
 
+# Print installed version
 dotnet --version
