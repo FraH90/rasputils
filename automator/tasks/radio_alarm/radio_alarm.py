7bc6ad4967efe6ae0bdddfc5fb3085b7784c646e
frah90 <frah.221190@gmail.com>
2024-10-22 16:14:41 +0200

    Fix: probably fixed double running istance issue for radio_alarm.py

--- automator/tasks/radio_alarm/radio_alarm.py
+++ automator/tasks/radio_alarm/radio_alarm.py
@@ -5,6 +5,7 @@ import time
 import vlc
 import os
 import pexpect
+import threading
 
 CURRENT_TASK_DIR = os.path.dirname(__file__)
 
@@ -78,9 +79,24 @@ class BluetoothHandler:
             return False
 
 class RadioPlayer:
+    # Implementing the singleton pattern for RadioPlayer ot ensure that only one istance of the player is created
+    # We also ensure that the automator properly manages threads and does not create multiple threads for the same task
+    # Moreover we'll add some logging
+    _instance = None
+    _lock = threading.Lock()
+
+    def __new__(cls, *args, **kwargs):
+        with cls._lock:
+            if cls._instance is None:
+                cls._instance = super(RadioPlayer, cls).__new__(cls)
+        return cls._instance
+    
     def __init__(self):
-        self.config = self.load_config()
-        self.radio_streams = self.load_radio_streams()
+        if not hasattr(self, 'initialized'):
+            self.config = self.load_config()
+            self.radio_streams = self.load_radio_streams()
+            self.is_playing = False  # Add a flag to check if the radio is playing
+            self.initialized = True
     
     def load_config(self):
         with open(CONFIG_FILE, 'r') as f:
@@ -91,19 +107,27 @@ class RadioPlayer:
             return json.load(f)
 
     def play_radio_for_one_hour(self, stream_url, radio_name):
-        # Increase VLC buffer and set audio output to ALSA
-        instance = vlc.Instance('--network-caching=3000', '--file-caching=3000', '--live-caching=3000', '--aout=alsa')
-        player = instance.media_player_new()
-        media = instance.media_new(stream_url)
-        player.set_media(media)
-        player.play()
-        
-        print(f"{time.strftime('%H:%M')} - Playing radio {radio_name}")
-        # Play for 1 hour (3600 seconds)
-        time.sleep(3600)
-        # Stop the player
-        player.stop()
-        print(f"{time.strftime('%H:%M')} - Stopped playing radio {radio_name}")
+        if self.is_playing:
+            print("Radio is already playing. Skipping new play request.")
+            return
+
+        self.is_playing = True  # Set the flag to True when starting to play
+        try:
+            # Increase VLC buffer and set audio output to ALSA
+            instance = vlc.Instance('--network-caching=3000', '--file-caching=3000', '--live-caching=3000', '--aout=alsa')
+            player = instance.media_player_new()
+            media = instance.media_new(stream_url)
+            player.set_media(media)
+            player.play()
+
+            print(f"{time.strftime('%H:%M')} - Playing radio {radio_name}")
+            # Play for 1 hour (3600 seconds)
+            time.sleep(3600)
+            # Stop the player
+            player.stop()
+            print(f"{time.strftime('%H:%M')} - Stopped playing radio {radio_name}")
+        finally:
+            self.is_playing = False  # Reset the flag when done playing
 
     def start(self):
         bluetooth_mac_address = self.config['bluetooth_mac_address']
@@ -118,10 +142,30 @@ class RadioPlayer:
         else:
             print("Failed to connect to Bluetooth speaker. Exiting.")
 
+
+def check_if_already_running():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    pid_file = os.path.join(script_dir, 'radio_alarm.pid')
+    if os.path.isfile(pid_file):
+        print("Another instance is already running.")
+        sys.exit()
+    with open(pid_file, 'w') as f:
+        f.write(str(os.getpid()))
+
+def delete_pid_file():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    pid_file = os.path.join(script_dir, 'radio_alarm.pid')
+    if os.path.isfile(pid_file):
+        os.remove(pid_file)
+
 # Entry point of the program
 def main():
-    radio_player = RadioPlayer()
-    radio_player.start()
+    check_if_already_running()
+    try:
+        radio_player = RadioPlayer()
+        radio_player.start()
+    finally:
+        delete_pid_file()
 
 # This is if we want to run the script as a task
 def thread_loop():
